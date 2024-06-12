852a5060738f95b043115a3d451a3d3428197cd7
talimannek <talimannek@gmail.com>
2024-06-12 05:53:06 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+Kolichestvo_vypolnennyh_DZ=12
+Kolichestvo_zatrachennyh_chasov=1.5
+Nazvanie_kursa='Pyton'
+Vremya_na_odno_zadanie=Kolichestvo_zatrachennyh_chasov/Kolichestvo_vypolnennyh_DZ
+print("Курс:",Nazvanie_kursa,", всего задач:",Kolichestvo_vypolnennyh_DZ,", затрачено часов:",Kolichestvo_zatrachennyh_chasov,", среднее время выполнения",Vremya_na_odno_zadanie,"часа.")
