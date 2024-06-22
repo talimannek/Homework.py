8b08f78eb2520d3757a2a6ddf9aefefa05ff1515
talimannek <talimannek@gmail.com>
2024-06-22 12:38:21 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,12 @@
+immutable_var=(1969,1970,1971,[1987,1988,1989,1990,1991,1992,1993],'обучение в институте',True,False)
+print(immutable_var)
+mutable_list=[1987,1988,1989,1990,1991,1992,1993]
+mutable_list[0]=2023
+mutable_list[1]=2024
+mutable_list.append(2025)
+mutable_list.extend('word1')
+mutable_list.extend('word2')
+mutable_list.extend(['word3'])
+mutable_list.remove('1')
+mutable_list.remove(2025)
+print(mutable_list)
